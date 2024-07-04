0ecb03d37e70bb742e480652003c81da35aebacb
tomoya0220-gz <tomoya02201998@gmail.com>
2024-07-04 15:42:30 +0900

    表示メッセの追加

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,11 @@ export default function Game() {
     }
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        {move === currentMove ? (
+          <span>You are at move #{move}</span>
+        ) : (
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        )}
       </li>
     );
   });
